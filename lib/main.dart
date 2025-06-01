e98375bd39820b025b2e060bc11c67a6d6b49aec
Abdul Rahman <abdulrahmanmlg744@gmail.com>
2025-06-01 20:21:11 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:coffee_shop_ui/view/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Coffee Shop',
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+        home: const HomeScreen());
+  }
+}
